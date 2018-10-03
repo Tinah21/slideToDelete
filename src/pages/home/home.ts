ca08f2627d6b6536c1656b5715787552ed766c9b
Tinah21 <clementine.lira@gmail.com>
2018-10-03 15:26:27 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,48 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items = [];
+i = 0;
 
   constructor(public navCtrl: NavController) {
+    this.items = [
+      {title: 'Madeas witness protection'},
+      {title: 'For better or worse'},
+      {title: 'The marriage counselor'},
+      {title: 'The single moms club'},
+      {title: 'Diary of a black mad woman'}
+  ];
 
   }
+
+  removeItem(item){
+ 
+    for(this.i = 0; this.i < this.items.length; this.i++) {
+ 
+      if(this.items[this.i] == item){
+        this.items.splice(this.i, 1);
+      }
+ 
+    }
+
+    
+ 
+  }
+  doRefresh(refresher){
+    this.items = [
+      {title: 'Madeas witness protection'},
+      {title: 'For better or worse'},
+      {title: 'The marriage counselor'},
+      {title: 'The single moms club'},
+      {title: 'Diary of a black mad woman'}
+  ];
+    
+ setTimeout(() => {
+   console.log("Async operation has ended");
+   refresher.complete();
+ }, 2000);
+}
+ 
+
 
 }
